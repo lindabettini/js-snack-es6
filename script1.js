889edee216f3485f9afa1fc2f4fdedebdb069d94
lindabettini <linda.bettini@hotmail.it>
2022-01-22 15:28:33 +0100

    add scripts

---
+++ script1.js
@@ -0,0 +1,6 @@
+/* 
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+*/
--- script2.js
+++ script2.js
@@ -1,9 +1,4 @@
-/* 
-Snack 1
-Creare un array di oggetti:
-Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare ain console la bici con peso minore utilizzando destructuring e template literal
-Snack2
+/* Snack2
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà:
