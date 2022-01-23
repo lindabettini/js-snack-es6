227bbd4dd7057772375f24b4f0ef3c3ba472e94c
lindabettini <linda.bettini@hotmail.it>
2022-01-23 15:39:56 +0100

    Complete track (print in console)

--- script2.js
+++ script2.js
@@ -6,3 +6,41 @@ Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 BONUS
 Stampare in pagina oltre che in console! */
+
+const teams = [
+    {
+        nome: 'Milan',
+        punti: 0,
+        falliSubuti: 0,
+    },
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falliSubuti: 0,
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falliSubuti: 0,
+    },
+]
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min +1)) - min;
+
+for (let i = 0; i < teams.length; i++) {
+    const team = teams [i];
+
+    team.punti = getRandomNumber(1, 100);
+    team.falliSubuti = getRandomNumber(1, 50);
+}
+
+const newArray = [];
+
+for (let i = 0; i < teams.length; i++) {
+    const team = teams [i];
+    const {nome, falliSubuti} = team;
+    const newObject = { nome, falliSubuti};
+    newArray.push(newObject);
+}
+
+console.log(newArray);
