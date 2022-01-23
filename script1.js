c088e347743e6301251c78a8f2b384e9e86b76d1
lindabettini <linda.bettini@hotmail.it>
2022-01-23 15:23:17 +0100

    track complete (print in console)

--- script1.js
+++ script1.js
@@ -4,3 +4,31 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare ain console la bici con peso minore utilizzando destructuring e template literal
 */
+
+const bikes = [
+    {
+        name: 'Pinarello',
+        weight: 10,
+    },
+    {
+        name: 'Bianchi',
+        weight: 8,
+    },
+    {
+        name: 'Scott',
+        weight: 11,
+    }
+];
+
+// ° Preparo una variabile che conterra' l'oggetto più leggero 
+let lightestBike = bikes[0];
+console.log(lightestBike);
+
+// Le peso tutte per vedere quale pesa meno 
+for (let i = 0; i < bikes.length; i++) {
+    const bike = bikes[i]; 
+    const { weight } = bike; 
+    if (weight < lightestBike.weight) lightestBike = bike;
+}
+
+console.log(lightestBike);
