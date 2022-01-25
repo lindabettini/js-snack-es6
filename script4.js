189a1a41af7de78358f26651c64959ca60dedc57
lindabettini <linda.bettini@hotmail.it>
2022-01-25 23:38:37 +0100

    track complete

--- script4.js
+++ script4.js
@@ -6,3 +6,22 @@ ES (marco de iulio => Marco de iulio);
 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 BONUS  nei nomi sulle targhe l'iniziale di ogni parola deve essere maiuscola
 ES.: (marco de iulio  => Marco De Iulio) */
+
+let students = [
+    { name: 'Luisa', id: '457', grades: '78'},
+    { name: 'Linda', id: '457', grades: '78'},
+    { name: 'Lorenzo', id: '457', grades: '78'},
+    { name: 'Luigi', id: '457', grades: '78'},
+    { name: 'Ludovica', id: '457', grades: '78'},
+    { name: 'Lucrezia', id: '457', grades: '78'},
+];
+
+const targheStudenti = students.map((student) => {
+    return student.name; 
+});
+
+const highGradesStudent = students.filter((student) => student.grades > 70);
+
+const highIdAndGradesStudent = students.filter((student) => student.grades > 70 && student.id > 120);
+
+console.log(targheStudenti);
